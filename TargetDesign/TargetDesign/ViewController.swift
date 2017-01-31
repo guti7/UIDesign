b91b9018a84c1accf0c20d55a4fed2652e72cccf
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-31 12:09:37 -0800

    make center target circle

--- TargetDesign/TargetDesign/ViewController.swift
+++ TargetDesign/TargetDesign/ViewController.swift
@@ -15,17 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        baseView.layer.borderWidth = 1
+        baseView.layer.borderWidth = 2
         
         baseView.layer.cornerRadius = baseView.bounds.width / 2
         // current values for baseView
         print ("baseView:\n bounds: \(baseView.bounds)\n layer: \(baseView.layer)")
         
         let centerLayer = CALayer()
-        centerLayer.frame = baseView.bounds.insetBy(dx: 10, dy: 10) //CGRect(x: baseView.center.x, y: baseView.center.y, width: baseView.bounds.width / 3, height: baseView.bounds.width / 3)
+        centerLayer.frame = baseView.bounds.insetBy(dx: 90, dy: 90) //CGRect(x: baseView.center.x, y: baseView.center.y, width: baseView.bounds.width / 3, height: baseView.bounds.width / 3)
         
         centerLayer.backgroundColor = UIColor.red.cgColor
         centerLayer.cornerRadius = centerLayer.bounds.width / 2
+        
+        for i in 0...4 {
+            
+        }
         
         baseView.layer.addSublayer(centerLayer)
         print ("centerLayer:\n contentsRect: \(centerLayer.contentsRect)")
